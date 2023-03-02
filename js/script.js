87cd4d08f30976448959255128ecbd519c5fb9e0
simanto789 <hazrat.ali789@gmail.com>
2023-03-02 22:34:04 +0600

    add card in the main section

--- js/script.js
+++ js/script.js
@@ -1,12 +1,27 @@
-const loadAiHub = async() =>{
+const loadAiHub = async () => {
     const url = `https://openapi.programming-hero.com/api/ai/tools`
     const res = await fetch(url);
-    const data =await res.json();
+    const data = await res.json();
     displayAiHub(data.data.tools);
 }
 
-const displayAiHub = ai => {
-    console.log(ai);
+const displayAiHub = aiHub => {
+    const aiContainer = document.getElementById('ai-container')
+    aiHub.forEach(ai => {
+        const aiDiv = document.createElement('div');
+        aiDiv.classList.add('col');
+        aiDiv.innerHTML = `
+           <div class="card">
+              <img src="${ai.image}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-feature">Features</h5>
+                    
+                    <p class="card-text">This is a longer card with supporting text below as a naturallead-in to additional content. This content is a little bit longer.</p>
+                </div>
+            </div>
+    `;
+    aiContainer.appendChild(aiDiv);
+    })
 }
 
 loadAiHub();
